e0651fb65d75e61e2a81e9c9a138b88ad7767b66
Inglsmit <paul.inglsmit@gmail.com>
2022-04-20 13:52:32 +0300

    UPD: save all inner elements

--- src/image-list-item/save.js
+++ src/image-list-item/save.js
@@ -1,8 +1,27 @@
+// eslint-disable-next-line no-unused-vars
 import { __ } from '@wordpress/i18n';
-import { useBlockProps } from '@wordpress/block-editor';
+import { useBlockProps, RichText } from '@wordpress/block-editor';
 
-export default function save() {
+export default function Save({ attributes }) {
+	const { imgID, imgURL, imgAlt, title, description } = attributes;
+
 	return (
-		<p {...useBlockProps.save()}>{__('IMAGE INNER', 'cm-image-list')}</p>
+		<div {...useBlockProps.save()}>
+			{imgURL && (
+				<img
+					src={imgURL}
+					alt={imgAlt}
+					className={imgID ? `wp-image-${imgID}` : null}
+				/>
+			)}
+			{title && <RichText.Content tagName="h4" value={title} />}
+			{description && (
+				<RichText.Content
+					tagName="p"
+					className="description"
+					value={description}
+				/>
+			)}
+		</div>
 	);
 }
